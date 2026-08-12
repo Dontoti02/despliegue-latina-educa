7ca6b33abdf0b2b7be30dc0c7cd5d0eee0269f5c
Danilo Andres M <danilo.marcelo@andheuris.com>
2026-08-12 18:05:56 -0500

    fix: tenant central domain

--- config/tenancy.php
+++ config/tenancy.php
@@ -14,10 +14,7 @@ return [
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
     'central_domains' => [
-        'academico.dev',
-        'latinaeduca.com',
-        'latinaeduca.info',
-        'educalatina.pe'
+      'iestp-rioja.edu.pe'
     ],
 
     /**
